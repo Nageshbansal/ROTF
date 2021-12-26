6752697dc49882576ebd5a68432182a656669759
Nageshbansal <nageshbansal59@gmail.com>
2021-12-26 15:01:55 +0530

    deleted robot.urdf

--- rotf_control/scripts/pos_diffdrive.py
+++ rotf_control/scripts/pos_diffdrive.py
@@ -14,11 +14,7 @@ w_z = 0
 err = 0.0
 
 tim = 0.0
-<<<<<<< HEAD:rotf_control/src/scripts/pos_diffdrive.py
-kp , kd ,ki = 2.5 , 0.0 , 0.0 
-=======
 kp_x , kp_y ,kp_z = 2 , 1.0 , 1.0 
->>>>>>> 011e42d7bdcc9d3278b68972cb812411205b3967:rotf_control/scripts/pos_diffdrive.py
 prev_time = 0.0 
 dt = 0.0 
 
@@ -39,7 +35,7 @@ def cal_vel(msg):
     v_x = msg.twist.twist.linear.x
     v_y = msg.twist.twist.linear.y
     w_z =  msg.twist.twist.angular.z
-    # print(v_x,v_y,w_z)
+  
     
 
 def pid(v_x,v_y,w_z):
@@ -51,18 +47,6 @@ def pid(v_x,v_y,w_z):
    corr_z=0.0
    global tim
    tim = rospy.Time.now()
-<<<<<<< HEAD:rotf_control/src/scripts/pos_diffdrive.py
-   err_1 = v_x - 0.1
-   err_2 =  v_y - 0.0 
-   prev_error_1 =0.0
-   prev_error_2 =0.0
-
-  
-   corr_1 = kp*(err_1)
-   corr_2 = kp*err_2 
-   prev_error = err
-   return (corr_1,corr_2)
-=======
    err_x = v_x - 0.1
    err_y=v_y
    err_z=w_z
@@ -82,7 +66,6 @@ def pid(v_x,v_y,w_z):
    prev_error_y=err_y
    prev_error_z=err_z
    return (correction)
->>>>>>> 011e42d7bdcc9d3278b68972cb812411205b3967:rotf_control/scripts/pos_diffdrive.py
 
 
 rospy.init_node('pos_diffdrive')
@@ -101,7 +84,6 @@ d =5
 
 while not rospy.is_shutdown():
     x_goal=d
-    move.angular.z= 0
     if x_goal-x <= 0:
         
         move.linear.x = 0
@@ -112,22 +94,14 @@ while not rospy.is_shutdown():
     else:
         # move.linear.x =(x_goal-x)
         # move.linear.y = 0
-<<<<<<< HEAD:rotf_control/src/scripts/pos_diffdrive.py
-        corr_1 , corr_2 = pid(v_x,v_y)
-        move.linear.x =corr_1*-10
-        move.linear.y =corr_2*-10
-       
-        # print(move.linear.x)
-=======
         corr = pid(v_x,v_y,w_z)
         move.linear.x =corr[0]*(-10)
         move.linear.y=corr[1]*(-10)
         move.angular.z=corr[2]*(0.01)
         move.linear.y =0.0
         print(move.linear.x)
->>>>>>> 011e42d7bdcc9d3278b68972cb812411205b3967:rotf_control/scripts/pos_diffdrive.py
 
-    move.angular.z = 0.0   
+        
     pub.publish(move)
     rate.sleep()
 
